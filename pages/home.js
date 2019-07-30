fb293a637fdb67f8042021304519b81e2b6cdacd
suryavip <suryabejibun@gmail.com>
2019-07-30 17:51:48 +0700

    update

--- pages/home.js
+++ pages/home.js
@@ -1,33 +1,47 @@
 vipPaging.pageTemplate['home'] = {
 	import: [
-		'pages/embeddedHome.js',
+		//
 	],
-	preopening: (d) => {
-		if (!firebaseAuth.authCheck(true)) return false;
-		if (document.body.offsetWidth > 700) {
-			//wide mode, home is not needed (embeeded in group.js side bar)
-			if (d.direction === 'forward') go('group');
-			else if (d.direction === 'back') {
-				// if back from group, then keep going back
-				// else, open group
-				if (vipHistory.pageBeforeBack === 'group') window.history.go(-1);
-				else go('group');
-			}
-			return false;
-		}
-		return true;
-	},
+	preopening: () => firebaseAuth.authCheck(true),
 	opening: () => {
-		embeddedHome.opening();
+		//
 	},
 	innerHTML: () => `
 <div class="vipPaging-vLayout">
 	<div class="head"><div>
-		${embeddedHome.head()}
+		<div class="actionBar aPadding-10">
+			<div class="button" onclick="go('settings')" title="Grup-grup"><i class="fas fa-users"></i></div>
+			<div class="title center">Beranda</div>
+			<div class="profilePhoto" id="embeddedHomePhoto" onclick="embeddedHome.profilePopUp()" title="Profil"><i class="fas fa-user"></i></div>
+		</div>
 	</div></div>
 	<div class="body"><div><div class="maxWidthWrap-480 aPadding-20" style="text-align:center">
-		${embeddedHome.body()}
+		--
 	</div></div></div>
+	<div class="foot"><div>
+		<div class="actionBar aPadding-10">
+			<div class="vipPaging-subNavigator" onclick="vipPaging.subPage.change(this)" data-active="true">
+				<i class="fas fa-home"></i>
+				<h3>${gl('home')}</h3>
+			</div>
+			<div class="vipPaging-subNavigator" id="announcementTabBtn" onclick="vipPaging.subPage.change(this)">
+				<i class="fas fa-bullhorn"></i>
+				<h3>${gl('announcement')}</h3>
+			</div>
+			<div class="vipPaging-subNavigator" id="scheduleTabBtn" onclick="vipPaging.subPage.change(this)">
+				<i class="fas fa-clock"></i>
+				<h3>${gl('schedule')}</h3>
+			</div>
+			<div class="vipPaging-subNavigator" id="assignmentTabBtn" onclick="vipPaging.subPage.change(this)">
+				<i class="fas fa-tasks"></i>
+				<h3>${gl('assignment')}</h3>
+			</div>
+			<div class="vipPaging-subNavigator" id="examTabBtn" onclick="vipPaging.subPage.change(this)">
+				<i class="fas fa-flag-checkered"></i>
+				<h3>${gl('exam')}</h3>
+			</div>
+		</div>
+	</div></div>
 </div>
 `,
 	functions: {
